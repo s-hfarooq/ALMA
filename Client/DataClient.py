69f84e10f39bc02d504cbc00384f5160b74815d2
Hassan F <1hassanfarooq@gmail.com>
2020-11-24 16:28:40 -0600

    Started work on server

--- Client/DataClient.py
+++ Client/DataClient.py
@@ -1,8 +1,5 @@
-
-
 import socket
 import sys
-
 
 IP_ADDRESS = "192.168.0.237"
 IP_PORT = 10000
@@ -19,18 +16,11 @@ try:
 
     while True:
         # Send data
-        #message = b'This is the message.  It will be repeated.'
         message = input("message: ").encode()
         print('sending {!r}'.format(message))
         sock.sendall(message)
 
-        # Look for the response
-        # amount_received = 0
-        # amount_expected = len(message)
-        #
-        # while amount_received < amount_expected:
         data = sock.recv(16)
-        #amount_received += len(data)
         print('received {!r}'.format(data))
 
 finally:
--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # ALMA
 
-Python 2
-
 http://www.python-exemplary.com/index_en.php?inhalt_links=navigation_en.inc.php&inhalt_mitte=raspi/en/communication.inc.php
-
-
-https://dordnung.de/raspberrypi-ledstrip/
 
 https://github.com/michaeljtbrooks/raspiled
 
 kill: ps, kill -9 ID
 
-
 nohup python3 DataServer.py &
+
+
+
+
+
+https://www.amazon.com/LM2596-Converter-Module-Supply-1-23V-30V/dp/B008BHBEE0#customerReviews
+
+
+
+https://www.digikey.com/en/products/detail/OKI-78SR-5%2f1.5-W36-C/811-2196-5-ND/2259781?itemSeq=345251247
+
+
+PINOUT
+NONE
+NONE
+BLACK
+PURPLE
+GREY
+WHITE
+NONE
+GREEN
+BLUE
+NONE
+YELLOW
