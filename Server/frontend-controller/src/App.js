92065021495dc43f9f09e4d60ab81b7fce33ab51
Hassan F <1hassanfarooq@gmail.com>
2020-11-25 00:28:12 -0600

    handleChange switched to handleChangeComplete

--- Server/frontend-controller/src/App.js
+++ Server/frontend-controller/src/App.js
@@ -10,10 +10,15 @@ class App extends React.Component {
     background: '#fff'
   }
 
+  // runs once color stops changing
   handleChangeComplete = (color) => {
     this.setState({ background: color.hex });
+    let newColStr = "col " + color.rgb.r + " " + color.rgb.g + " " + color.rgb.b;
+    console.log(newColStr);
+    changeColor(newColStr);
   };
 
+  // runs everytime a color changes
   handleChange(color, event) {
     // color = {
     //   hex: '#333',
@@ -41,7 +46,7 @@ class App extends React.Component {
       <SketchPicker
         color={ this.state.background }
         onChangeComplete={ this.handleChangeComplete }
-        onChange={ this.handleChange }
+        // onChange={ this.handleChange }
       />
     );
   }
