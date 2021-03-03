0bf392c0fe22c07d5ccfed25bb121316167d7fdb
Hassan F <1hassanfarooq@gmail.com>
2021-03-03 01:29:28 -0600

    Trying live updated again

--- Server/frontend-controller/src/App.js
+++ Server/frontend-controller/src/App.js
@@ -15,6 +15,37 @@ class App extends React.Component {
   // Runs everytime a color changes
   handleChange = async (color, event) => {
     this.setState({ background: color.hex });
+
+    let option = this.state.selectedOption.value;
+    let newColStr = color.rgb.r + "-" + color.rgb.g + "-" + color.rgb.b;
+
+    // Figure out what output string ending needs to be
+    switch(option) {
+      case "1colceiling":
+        newColStr += "-1-1-0-";
+        break;
+      case "1colcouch":
+        newColStr += "-1-2-0-";
+        break;
+      case "2colceiling":
+        newColStr += "-2-1-0-";
+        break;
+      case "2colcouch":
+        newColStr += "-2-2-0-";
+        break;
+      case "bothceiling":
+        newColStr += "-0-1-0-";
+        break;
+      case "bothcouch":
+        newColStr += "-0-2-0-";
+        break;
+      default:
+        newColStr += "-0-0-0-";
+    }
+
+    console.log("Color string: " + newColStr);
+
+    await sendCommand(newColStr);
   }
 
   // Kill connection when colors aren't being changed
