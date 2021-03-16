05750848751ab9e1125b822b47b2f932b87732e1
Hassan F <1hassanfarooq@gmail.com>
2021-03-16 17:56:09 -0500

    latency bad v3

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,3 +1,42 @@
+// #!/usr/bin/env node
+//
+// const express = require('express');
+// const path = require('path');
+// const app = express(),
+//       bodyParser = require("body-parser");
+//       port = 3080;
+// var net = require('net');
+//
+// var lastSentTime = new Date();
+//
+// const spawn = require("child_process").spawn;
+//
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, '../frontend-controller/build')));
+//
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../frontend-controller/build/index.html'));
+// });
+//
+// app.post('/sendCommand', (req, res) => {
+//   console.log("COMMAND: " + req.body.color);
+//
+//   let currTime = new Date();
+//   if(currTime - lastSentTime > 400) {
+//     const pythonProcess = spawn('python3',["i2cpi.py", req.body.color]);
+//     lastSentTime = currTime;
+//   }
+//
+//   res.json({connection: "Sent"})
+// });
+//
+// app.listen(port, () => {
+//   console.log(`Server listening on the port::${port}`);
+// });
+
+
+
+
 #!/usr/bin/env node
 
 const express = require('express');
@@ -9,7 +48,8 @@ var net = require('net');
 
 var lastSentTime = new Date();
 
-const spawn = require("child_process").spawn;
+var spawn = require('child_process').spawn;
+var child = spawn('python3',["i2cpi.py"]);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend-controller/build')));
@@ -23,7 +63,10 @@ app.post('/sendCommand', (req, res) => {
 
   let currTime = new Date();
   if(currTime - lastSentTime > 400) {
-    const pythonProcess = spawn('python3',["i2cpi.py", req.body.color]);
+    //const pythonProcess = spawn('python3',["i2cpi.py", req.body.color]);
+
+    child.stdin.setEncoding('utf-8');
+    child.stdin.write(req.body.color + "\n");
     lastSentTime = currTime;
   }
 
