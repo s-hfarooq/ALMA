87b0ede55623dbec8cd54eaa6f26603aea2a6321
Hassan F <1hassanfarooq@gmail.com>
2021-03-16 18:02:18 -0500

    latency bad (but cleaned up)

--- Server/api/i2cpi.py
+++ Server/api/i2cpi.py
@@ -1,32 +1,3 @@
-# # -*- coding: utf-8 -*-
-# import smbus
-# import time
-# import sys
-# # for RPI version 1, use bus = smbus.SMBus(0)
-# bus = smbus.SMBus(1)
-#
-# # This is the address we setup in the Arduino Program
-# address = 0x04
-#
-# #http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
-# def writeData(value):
-#     byteValue = StringToBytes(value)
-#     bus.write_i2c_block_data(address,0x00,byteValue) #first byte is 0=command byte.. just is.
-#     return -1
-#
-#
-# def StringToBytes(val):
-#         retVal = []
-#         for c in val:
-#                 retVal.append(ord(c))
-#         return retVal
-#
-# args = list(sys.argv)
-# print('Sending...')
-# writeData("-" + args[1])
-
-
-
 # -*- coding: utf-8 -*-
 import smbus
 import time
@@ -50,14 +21,6 @@ def StringToBytes(val):
                 retVal.append(ord(c))
         return retVal
 
-
 while(True):
-    #args = list(sys.argv)
     txt = input("Cmd: ")
-    # strings = ['255-0-0-0-0-0-', '0-255-0-0-0-0-', '0-0-255-0-0-0-', '0-0-0-0-0-0-']
-    # for i in range(len(strings)):
-    #     print('Sending ', strings[i])
-    #     writeData("-" + strings[i])
-    #     time.sleep(0.5)
-
     writeData("-" + txt)
--- Server/api/server.js
+++ Server/api/server.js
@@ -1,44 +1,3 @@
-// #!/usr/bin/env node
-//
-// const express = require('express');
-// const path = require('path');
-// const app = express(),
-//       bodyParser = require("body-parser");
-//       port = 3080;
-// var net = require('net');
-//
-// var lastSentTime = new Date();
-//
-// const spawn = require("child_process").spawn;
-//
-// app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, '../frontend-controller/build')));
-//
-// app.get('/', (req,res) => {
-//   res.sendFile(path.join(__dirname, '../frontend-controller/build/index.html'));
-// });
-//
-// app.post('/sendCommand', (req, res) => {
-//   console.log("COMMAND: " + req.body.color);
-//
-//   let currTime = new Date();
-//   if(currTime - lastSentTime > 400) {
-//     const pythonProcess = spawn('python3',["i2cpi.py", req.body.color]);
-//     lastSentTime = currTime;
-//   }
-//
-//   res.json({connection: "Sent"})
-// });
-//
-// app.listen(port, () => {
-//   console.log(`Server listening on the port::${port}`);
-// });
-
-
-
-
-#!/usr/bin/env node
-
 const express = require('express');
 const path = require('path');
 const app = express(),
@@ -62,9 +21,7 @@ app.post('/sendCommand', (req, res) => {
   console.log("COMMAND: " + req.body.color);
 
   let currTime = new Date();
-  if(currTime - lastSentTime > 400) {
-    //const pythonProcess = spawn('python3',["i2cpi.py", req.body.color]);
-
+  if(currTime - lastSentTime > 250) {
     child.stdin.setEncoding('utf-8');
     child.stdin.write(req.body.color + "\n");
     lastSentTime = currTime;
